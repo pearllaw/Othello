290b4d005ce75728d711e6d402ce034d8830d615
Pearl Law <pearllynnelaw@gmail.com>
2021-05-04 16:24:30 -0700

    Remove mobility function and update evaluation function

--- othello.py
+++ othello.py
@@ -100,7 +100,7 @@ class Othello(Game):
         """Evaluation function that determines the goodness/value of a 
         position in the current board state."""
         board = state.board
-        return self.disk_difference(board) + 1000*self.corners_occupied(board) + 10*self.corner_closeness(board) + self.mobility(board)
+        return self.disk_difference(board) + 1000*self.corners_occupied(board) + 10*self.corner_closeness(board)
 
     def disk_difference(self, board):
         """Captures difference in disks on the board between B - human player 
@@ -136,20 +136,6 @@ class Othello(Game):
             if board.get((0, col)) == 'W': w += 1
             if board.get((7, col)) == 'W': w += 1
         return w
-
-    def mobility(self, state):
-        """Captures relative difference in mobility (possible moves) for max and 
-        min player."""
-        opponent = self.opponent(state.to_move)
-        opponent_state = GameState(to_move=opponent,
-            utility=self.compute_utility(opponent, state.board),
-            board=state.board, moves=[])
-        black_moves = len(self.actions(state))
-        white_moves = len(self.actions(opponent_state))
-        if black_moves + white_moves == 0:
-            return 0
-        else:
-            return white_moves
         
     def terminal_test(self, state):
         """A state is terminal if it is won or neither player has any valid move."""
